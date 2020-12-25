5abc360b426c668b47795b9f89871e2ffe7f35e5
Milan Gada <milanga@microsoft.com>
2020-12-25 08:20:07 -0800

    updates

---
+++ edge-device-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script creates a new Edge Device in given IoT Hub and connects it to a Existing VM with IoT Edge runtime installed on it.
+
+echo "Registering edge device $1 with IoT Hub $2"
